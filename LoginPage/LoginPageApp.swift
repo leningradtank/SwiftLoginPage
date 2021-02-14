86db55c000450a5fdfe2bf7ae3d68d1a0d5f0b4d
ridersamz2000 <47441021+ridersamz2000@users.noreply.github.com>
2021-02-13 22:30:14 -0600

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by sairaj patil on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
